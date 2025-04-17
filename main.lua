8e4048a60c134a3dc9a16c46070fe1dab297e61c
Sam F <github@ipto.space>
2025-04-17 00:39:46 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,9 +2,9 @@
 getgenv().Config = {
     Webhook = "Webhook URL",
     ScriptURL = "https://russia.1ms.network/rblx/webhook.lua",
-    AntiAFKURL = "https://russia.1ms.network/rblx/antiafk.lua"
+    --AntiAFKURL = "https://russia.1ms.network/rblx/antiafk.lua"
 }
 
 print("Starting: Hatching Scripts")
 loadstring(game:HttpGet(getgenv().Config.ScriptURL))()
-loadstring(game:HttpGet(getgenv().Config.AntiAFKURL))()
+--loadstring(game:HttpGet(getgenv().Config.AntiAFKURL))()
