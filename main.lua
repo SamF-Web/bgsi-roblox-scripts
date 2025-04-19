7d813cadafc158b8ecc9e619f0778a8052ece4d6
Sam F <github@ipto.space>
2025-04-19 14:19:14 -0700

    Main Update | Rev. 1.0.4

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 --// Loader Rev. 1.0.4
 getgenv().Config = {
-    Webhook = "https://discord.com/api/webhooks/1362284955241021440/ISVUyf5Aws3G7sT1uMbxbUvB4fyEGvDqTq7DtQwqe11-qERRUoYG0zCcOQytzlFbjgC9",
+    Webhook = "https://discord.com/api/webhooks/",
     ScriptURL = "https://russia.1ms.network/rblx/webhook.lua",
     AntiAFKURL = "https://russia.1ms.network/rblx/antiafk.lua",
 }
