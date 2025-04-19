bf147808c5303e08bdbecf54801fbf11b5161a25
Sam F <github@ipto.space>
2025-04-19 13:49:09 -0700

    Main Update | Rev. 1.0.4
    Added Keybinds for AntiAFK [Still toggled on, on inject]

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
---// Loader
- getgenv().Config = {
-     Webhook = "https://discord.com/api/webhooks/",
-     ScriptURL = "https://russia.1ms.network/rblx/webhook.lua",
-     AntiAFKURL = "https://russia.1ms.network/rblx/antiafk.lua"
- }
- 
- print("Starting: Hatching Scripts")
- loadstring(game:HttpGet(getgenv().Config.ScriptURL))()
- loadstring(game:HttpGet(getgenv().Config.AntiAFKURL))()
+--// Loader Rev. 1.0.4
+getgenv().Config = {
+    Webhook = "https://discord.com/api/webhooks/",
+    ScriptURL = "https://russia.1ms.network/rblx/webhook.lua",
+    AntiAFKURL = "https://russia.1ms.network/rblx/antiafk.lua",
+
+    --// Config | Change the key in the "" to update keybind
+    AntiAFKBind = "R"
+}
+
+print("Starting: Hatching Scripts")
+loadstring(game:HttpGet(getgenv().Config.ScriptURL))()
+loadstring(game:HttpGet(getgenv().Config.AntiAFKURL))()
