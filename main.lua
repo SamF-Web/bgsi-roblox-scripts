99a91bf4fb2c305c534b2b492431aaaf162fc634
Sam F <github@ipto.space>
2025-05-15 16:53:52 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 --// Loader Rev. 1.0.5
 getgenv().Config = {
     Webhook = "https://discord.com/api/webhooks/",
-    ScriptURL = "https://cdn.1ms.network/rblx/webhook.lua",
-    AntiAFKURL = "https://cdn.1ms.network/rblx/antiafk.lua",
+    ScriptURL = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/webhook.lua",
+    AntiAFKURL = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/antiafk.lua",
 }
 
 print("Starting: Hatching Scripts")
