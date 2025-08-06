81e6a87fa96b3bb0b9916edf95f62274cd86e7ea
Sam F <github@ipto.space>
2025-08-06 15:12:00 -0400

    BGSI Main Update

--- main.lua
+++ main.lua
@@ -1,10 +1,22 @@
---// Loader Rev. 1.0.5
+--// BGSI Script | Rev. 1.0.6 | BGSI Build v9986
 getgenv().Config = {
+    --// General Settings
+    ClaimPlaytime = true,
+    AntiAFK = true,
+
+    --// Webhook Settings
+    Webhook_enabled = true,
+    Discord_ID = DISCORDID,
     Webhook = "https://discord.com/api/webhooks/",
-    ScriptURL = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/webhook.lua",
-    AntiAFKURL = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/antiafk.lua",
+    Ignore_AutoDeleted = true,
+    Secret_Only = false,
+
+    --// Script Files
+    WebhookScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/webhook.lua",
+    AntiAFKScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/antiafk.lua",
+    PlaytimeScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/playtime.lua"
 }
 
-print("Starting: Hatching Scripts")
-loadstring(game:HttpGet(getgenv().Config.ScriptURL))()
-loadstring(game:HttpGet(getgenv().Config.AntiAFKURL))()
+loadstring(game:HttpGet(getgenv().Config.WebhookScript))()
+loadstring(game:HttpGet(getgenv().Config.AntiAFKScript))()
+loadstring(game:HttpGet(getgenv().Config.PlaytimeScript))()
