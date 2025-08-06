e719d677723d6a49adf2daf57d9b9138e82b16b6
Sam F <github@ipto.space>
2025-08-06 15:11:00 -0400

    Webhook Config Update

--- webhook.lua
+++ webhook.lua
@@ -1,4 +1,3 @@
--- Check for supported HTTP request
 local httpRequest =
     (syn and syn.request) or
     (http and http.request) or
@@ -15,31 +14,6 @@ local player = game.Players.LocalPlayer
 local httpService = game:GetService("HttpService")
 local runService = game:GetService("RunService")
 
-local petImages = {
-    -- Base Legendary Images
-    ["Emerald Golem"] = "https://static.wikia.nocookie.net/bgs-infinity/images/8/8f/Emerald_Golem.png/revision/latest?cb=20250412234012",
-    ["Inferno Dragon"] = "https://static.wikia.nocookie.net/bgs-infinity/images/0/06/Inferno_Dragon.png/revision/latest?cb=20250412205317",
-    ["Flying Pig"] = "https://static.wikia.nocookie.net/bgs-infinity/images/1/18/Flying_Pig.png/revision/latest?cb=20250412142625",
-    ["Unicorn"] = "https://static.wikia.nocookie.net/bgs-infinity/images/7/7e/Unicorn.png/revision/latest?cb=20250412233524",
-    ["Lunar Serpent"] = "https://static.wikia.nocookie.net/bgs-infinity/images/5/50/Lunar_Serpent.png/revision/latest?cb=20250413002818",
-    ["Electra"] = "https://static.wikia.nocookie.net/bgs-infinity/images/a/a0/Electra.png/revision/latest?cb=20250412204328",
-    ["Dark Phoenix"] = "https://static.wikia.nocookie.net/bgs-infinity/images/1/1c/Dark_Phoenix.png/revision/latest?cb=20250413001114",
-    ["Neon Elemental"] = "https://static.wikia.nocookie.net/bgs-infinity/images/1/11/Neon_Elemental.png/revision/latest?cb=20250413001526",
-    ["NULLVoid"] = "https://static.wikia.nocookie.net/bgs-infinity/images/9/98/NULLVoid.png/revision/latest?cb=20250413001555",
-    ["Inferno Cube"] = "https://static.wikia.nocookie.net/bgs-infinity/images/0/0d/Inferno_Cube.png/revision/latest?cb=20250413010729",
-    ["Virus"] = "https://static.wikia.nocookie.net/bgs-infinity/images/1/1d/Virus.png/revision/latest?cb=20250412214808",
-    ["Green Hydra"] = "https://static.wikia.nocookie.net/bgs-infinity/images/b/bc/Green_Hydra.png/revision/latest?cb=20250412170659",
-    ["Demonic Hydra"] = "https://static.wikia.nocookie.net/bgs-infinity/images/1/17/Demonic_Hydra.png/revision/latest?cb=20250412170659",
-    ["Hexarium"] = "https://static.wikia.nocookie.net/bgs-infinity/images/8/89/Hexarium.png/revision/latest?cb=20250413010434",
-    ["Rainbow Shock"] = "https://static.wikia.nocookie.net/bgs-infinity/images/c/c6/Rainbow_Shock.png/revision/latest?cb=20250413010157",
-    ["Sigma Serpent"] = "https://static.wikia.nocookie.net/bgs-infinity/images/3/3d/Sigma_Serpent.png/revision/latest?cb=20250414121909",
-    ["Manarium"] = "https://static.wikia.nocookie.net/bgs-infinity/images/5/5d/Manarium.png/revision/latest?cb=20250414121813",
-    ["MAN FACE GOD"] = "https://static.wikia.nocookie.net/bgs-infinity/images/1/1b/MAN_FACE_GOD.png/revision/latest?cb=20250414020316",
-    ["The Overlord"] = "https://static.wikia.nocookie.net/bgs-infinity/images/c/c0/The_Overlord.png/revision/latest?cb=20250413130318",
-    ["King Doggy"] = "https://static.wikia.nocookie.net/bgs-infinity/images/a/a8/King_Doggy.png/revision/latest?cb=20250412152038",
-
-}
-
 local fallbackImage = "https://static.wikia.nocookie.net/bgs-infinity/images/7/73/Common_Pet.png"
 local recentFrames = {}
 
@@ -52,8 +26,11 @@ local function isRecentFrame(frame)
     return false
 end
 
-local function sendWebhook(name, rarity, shiny, image, chance)
-    -- Only send if chance is in "x in y" format
+local function sendWebhook(name, rarity, shiny, chance)
+    if not getgenv().Config.Webhook_enabled then
+        return
+    end
+
     if chance and not string.find(chance, " in ", 1, true) then
         return
     end
@@ -87,17 +64,20 @@ local function sendWebhook(name, rarity, shiny, image, chance)
         end
     end
 
+    local discordID = tostring(getgenv().Config.Discord_ID)
+    local mention = ""
+    if discordID and discordID ~= "" then
+        mention = "<@" .. discordID .. ">"
+    end
+
     local data = {
-        ["content"] = "@everyone",
+        ["content"] = mention,
         ["embeds"] = {{
             ["title"] = baseTitle,
             ["description"] = desc,
             ["color"] = 28927,
             ["footer"] = {
                 ["text"] = "Pet Hatched By: " .. player.Name .. " | 🥚 Total Hatches: " .. totalHatches
-            },
-            ["thumbnail"] = {
-                ["url"] = image or fallbackImage
             }
         }}
     }
@@ -111,7 +91,6 @@ local function sendWebhook(name, rarity, shiny, image, chance)
         Body = httpService:JSONEncode(data)
     })
 end
-
 
 local function monitorHatch()
     local gui = player:FindFirstChild("PlayerGui") and player.PlayerGui:FindFirstChild("ScreenGui")
@@ -130,14 +109,16 @@ local function monitorHatch()
 
             if rarityLower:find("legendary") or rarityLower:find("secret") or rarityLower:find("mythic") then
                 if not isRecentFrame(frame) then
-                    local image = petImages[name] or fallbackImage
-                    sendWebhook(name, rarity, shiny, image, chance)
-                    print("📤 Sent webhook for:", name)
+                    sendWebhook(name, rarity, shiny, chance)
                 end
             end
         end
     end
 end
 
-runService.RenderStepped:Connect(monitorHatch)
-print("Running: Webhook Alerts")
+if getgenv().Config.Webhook_enabled then
+    runService.RenderStepped:Connect(monitorHatch)
+    print("Running: Webhook Alerts")
+else
+    print("Webhook Alerts are disabled in the config.")
+end
