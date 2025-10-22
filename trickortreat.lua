7c5bfb35dbacd3be9ad4f3d8f84426a210437569
Sam <205131942+SamF-Web@users.noreply.github.com>
2025-10-22 09:43:12 -0400

    Finial Fix, Restored Values.

--- trickortreat.lua
+++ trickortreat.lua
@@ -29,7 +29,7 @@ local function startTrickOrTreatLoop()
         while true do
             for _, pos in ipairs(positions) do
                 dropOnto(pos)
-                task.wait(3.5)
+                task.wait(5)
             end
         end
     end)
