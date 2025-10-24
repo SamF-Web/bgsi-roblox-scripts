802fbeed4f08b4381a6e79b3fceb41d6dfafb58b
Sam <205131942+SamF-Web@users.noreply.github.com>
2025-10-24 19:47:02 -0400

    Trick or treat update

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
---// BGSI Script | Rev. 1.1.1 | BGSI Build v10068
+--// BGSI Script | Rev. 1.1.2 | BGSI Build v10077
 getgenv().Config = {
     --// General Settings
     ClaimPlaytime = true,
@@ -9,16 +9,17 @@ getgenv().Config = {
     --// Event Scripts
     EventZone = "First", --// First, Second, Third, Fourth
     TrickOrTreat = true, --// You Must be in the halloween world for it to start.
+    ToTWebhook = "https://discord.com/api/webhooks/",
 
     --// Webhook Settings
     Webhook_enabled = true,
-    Discord_ID = "discordid",
+    Discord_ID = "DISCORDID",
     Webhook = "https://discord.com/api/webhooks/",
     Ignore_AutoDeleted = true,
     Secret_Only = false,
 
     --// Anti Scam Settings
-    Allowed_User = "USERNAME", --// Set this as the account that could trade with you.
+    Allowed_User = "ROBLOXNAME", --// Set this as the account that could trade with you.
     AntiTrade = true,
 
     --// Performance Boost
@@ -32,7 +33,7 @@ getgenv().Config = {
     SeasonPassScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/seasonpass.lua",
     PotionScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/potioncraft.lua",
     BoosterScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/booster.lua",
-    TrickOrTreatScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/trickortreat.lua",
+    TrickOrTreatScript = "https://raw.bgsi-botting.xyz/v10077/events/trickortreat.lua",
 }
 
 loadstring(game:HttpGet(getgenv().Config.WebhookScript))()
