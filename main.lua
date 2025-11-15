96bc98c46daa2817d8003f5cb2cf189c4913bbaf
Sam <205131942+SamF-Web@users.noreply.github.com>
2025-11-14 21:58:42 -0500

    Rev 1.1.7

--- main.lua
+++ main.lua
@@ -1,53 +1,53 @@
---// BGSI Script | Rev. 1.1.3 | BGSI Build v10077
+--// BGSI Script | Rev. 1.1.7 | BGSI Build v10134
 getgenv().Config = {
     --// General Settings
     ClaimPlaytime = true,
-    MaxPotionCraft = true,
+    MaxPotionCraft = false,
     SeasonPassClaim = true,
     AntiAFK = true,
 
-    --// Event Scripts
-    AutoBuyEventShop = true, --// Will auto buy the normal halloween shop [NOT THE SHOP WITH THE SECRET]
-    EventZone = "First", --// First, Second, Third, Fourth
-    TrickOrTreat = false,
+    --// Auto Potion
+    InfinityElixir = true, --// Will use 1 every 6min
+    SecretElixir = true, --// Will use 1 every 12min
+    EggElixir = true, --// will use 1 every 25min
+    LuckyInf = true, --// Will use 1 every 37min
+    SpeedInf = true, --// Will use 1 every 37min
+    MythicInf = true, --// Will use 1 every 37min
 
-    --// Event Minigame
-    EventMinigame = true, --// set to false if you want to trick or treat farm
-    Mode = "Insane", --// Easy, Medium, Hard, insane
-    UseTickets = true, --// uses super tickets [if false will wait the cooldown]
-
+    --// OG Event
+    OGSpin = true,
 
     --// Webhook Settings
     Webhook_enabled = true,
-    Discord_ID = "DiscordID",
+    Discord_ID = "discordid",
     Webhook = "https://discord.com/api/webhooks/",
     Ignore_AutoDeleted = true,
     Secret_Only = false,
 
-    --// Anti Scam Settings
-    Allowed_User = "ROBLOXNAME", --// Set this as the account that could trade with you.
-    AntiTrade = true,
-
     --// Performance Boost
     FPS = "5",
-    LowGraphics = false,
+    LowGraphics = true,
+
+    --// Anti Scam Settings
+    Allowed_User = "ChangeMe",
+    AntiTrade = true,
 
     --// Script Files
-    WebhookScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/webhook.lua",
-    AntiAFKScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/antiafk.lua",
-    PlaytimeScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/playtime.lua",
-    SeasonPassScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/seasonpass.lua",
-    PotionScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/potioncraft.lua",
-    BoosterScript = "https://raw.githubusercontent.com/SamF-Web/bgsi-roblox-scripts/refs/heads/main/booster.lua",
-    TrickOrTreatScript = "https://raw.bgsi-botting.xyz/v10077/events/trickortreat.lua",
-    MinigameScript = "https://raw.bgsi-botting.xyz/v10077/events/minigame.lua",
+    WebhookScript = "https://raw.bgsi-botting.xyz/core/webhook.lua",
+    AutoPotionScript = "https://raw.bgsi-botting.xyz/core/autopotion.lua",
+    OGSpinScript = "https://raw.bgsi-botting.xyz/v10134/events/ogspin.lua",
+    AntiAFKScript = "https://raw.bgsi-botting.xyz/core/antiafk.lua",
+    PlaytimeScript = "https://raw.bgsi-botting.xyz/core/playtime.lua",
+    SeasonPassScript = "https://raw.bgsi-botting.xyz/core/seasonpass.lua",
+    PotionScript = "https://raw.bgsi-botting.xyz/core/potioncraft.lua",
+    BoosterScript = "https://raw.bgsi-botting.xyz/core/booster.lua",
 }
 
 loadstring(game:HttpGet(getgenv().Config.WebhookScript))()
+loadstring(game:HttpGet(getgenv().Config.AutoPotionScript))()
+loadstring(game:HttpGet(getgenv().Config.OGSpinScript))()
 loadstring(game:HttpGet(getgenv().Config.PlaytimeScript))()
 loadstring(game:HttpGet(getgenv().Config.SeasonPassScript))()
 loadstring(game:HttpGet(getgenv().Config.PotionScript))()
-loadstring(game:HttpGet(getgenv().Config.TrickOrTreatScript))()
-loadstring(game:HttpGet(getgenv().Config.MinigameScript))()
 loadstring(game:HttpGet(getgenv().Config.AntiAFKScript))()
 loadstring(game:HttpGet(getgenv().Config.BoosterScript))()
